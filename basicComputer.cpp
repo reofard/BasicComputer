d7d682f6ee816a92fdd81a405a55605c42a2f2f8
Reofard <6590f1@naver.com>
2021-10-28 14:48:55 +0900

    에러 수정

--- basicComputer.cpp
+++ basicComputer.cpp
@@ -8,113 +8,116 @@ using namespace std;
 
 //메모리에 데이터를 집어넣는 함수
 //매개변수 (배열의 위치, 값)
-void inputData(int position, word data) { }
+void inputData(int position, word data) {}
 
 //명령어 분리 (opcode 와 address)
 string decodeInstruction(word instruction)
 {
 	// Instruction Register에 명령어 저장
 	IR = instruction;
-	
+
 	// 명령어 타입 추출
-	//T2
+	// T2
 	byte type = (byte)(IR >> 12);
 	bool I = type | 8;
 
-	//DECODE
-	// 구분해낸 명령어 문자열을 통해 명령어 형식별로 결과를 출력함
+	// DECODE
+	//  구분해낸 명령어 문자열을 통해 명령어 형식별로 결과를 출력함
 	switch (type)
 	{
 
-		//Opcode = 111, I = 0
-		case 0x7:
-			cout << " 02. 명령어 형식 = 'Register' reference operation" << endl;
-			cout << " 03. Symbol = " << HexToString(IR) << endl;
-			return HexToString(IR);
+	// Opcode = 111, I = 0
+	case 0x7:
+		cout << " 02. 명령어 형식 = 'Register' reference operation" << endl;
+		cout << " 03. Symbol = " << HexToString(IR) << endl;
+		return HexToString(IR);
 
-		//Opcode = 111, I = 1
-		case 0xf:
-			cout << " 02. 명령어 형식 = 'I/O' operation" << endl;
-			cout << " 03. Symbol = " << HexToString(IR) << endl;
-			return HexToString(IR);
+	// Opcode = 111, I = 1
+	case 0xf:
+		cout << " 02. 명령어 형식 = 'I/O' operation" << endl;
+		cout << " 03. Symbol = " << HexToString(IR) << endl;
+		return HexToString(IR);
 
-		//Opcode = 000~110, I = 0, 1
-		default:
-			cout << " 02. 명령어 형식 = 'Memory' reference operation" << endl;
-			cout << " 03. Symbol = " << mHexToString(type) << endl;
+	// Opcode = 000~110, I = 0, 1
+	default:
+		cout << " 02. 명령어 형식 = 'Memory' reference operation" << endl;
+		cout << " 03. Symbol = " << mHexToString(type) << endl;
 
-			// 주소모드를 나타내는 4비트를 밀어버리고 12비트의 Address만 남김
-			if(I==1)
-			{
-				AR=(IR << 4);
-				AR=MEMORY[AR];
-			}
-			else AR = (IR << 4);
-			cout << " 04. Address = " << std::hex << (AR / 16) << "H" << endl;
+		// 주소모드를 나타내는 4비트를 밀어버리고 12비트의 Address만 남김
+		if (I == 1)
+		{
+			AR = (IR << 4);
+			AR = MEMORY[AR];
+		}
+		else
+			AR = (IR << 4);
+		cout << " 04. Address = " << std::hex << (AR / 16) << "H" << endl;
 
-			return mHexToString(type);
+		return mHexToString(type);
 	}
 }
-void executeInstruction(string symbol)
-{
-	switch(symbol)
-	{
-		case "AND":
-			break;
-		case "ADD":
-			break;
-		case "LDA":
-			break;
-		case "STA":
-			break;
-		case "BUN":
-			break;
-		case "BSA":
-			break;
-		case "ISZ":
-			break;
-		case "CLA":
-			break;
-		case "CLE":
-			break;
-		case "CMA":
-			break;
-		case "CME":
-			break;
-		case "CIR":
-			break;
-		case "CIL":
-			break;
-		case "INC":
-			break;
-		case "SPA":
-			break;
-		case "SNA":
-			break;
-		case "SZA":
-			break;
-		case "SZE":
-			break;
-		case "HLT":
-			break;
-		default:
-			cout << "I/O 명령어" <<endl
-	}
-}
-//CMA명령어 처리 함수
+
+// CMA명령어 처리 함수
 void CMA()
 {
-	AC=~AC;
+	AC = ~AC;
 }
-//SPA명령어 처리 함수
+// SPA명령어 처리 함수
 void SPA()
 {
-	if((AC>>15)==0)PC=PC+1;
+	if ((AC >> 15) == 0)
+		PC = PC + 1;
 }
-//HLT명령어 처리 함수
+// HLT명령어 처리 함수
 void HLT()
 {
 	S = false;
+}
+
+void executeInstruction(string symbol)
+{
+	if ("AND" == symbol)
+		;
+	else if ("ADD" == symbol)
+		;
+	else if ("LDA" == symbol)
+		;
+	else if ("STA" == symbol)
+		;
+	else if ("BUN" == symbol)
+		;
+	else if ("BSA" == symbol)
+		;
+	else if ("ISZ" == symbol)
+		;
+	else if ("CLA" == symbol)
+		;
+	else if ("CLE" == symbol)
+		;
+	else if ("CMA" == symbol)
+		CMA();
+	else if ("CME" == symbol)
+		;
+	else if ("CIR" == symbol)
+		;
+	else if ("CIL" == symbol)
+		;
+	else if ("INC" == symbol)
+		;
+	else if ("SPA" == symbol)
+		SPA();
+	else if ("SNA" == symbol)
+		;
+	else if ("SZA" == symbol)
+		;
+	else if ("SZE" == symbol)
+		;
+	else if ("HLT" == symbol)
+		HLT();
+	else
+		cout << "I/O 명령어" << endl;
+
+	//메모리 및 레지스터 상태 출력
 }
 
 void init()
@@ -135,11 +138,11 @@ int main()
 	MEMORY[0] = (word)0xf800;
 	MEMORY[1] = (word)0x7001;
 
-	while(S)
+	while (S)
 	{
-		//FETCH
-		//T0
-		AR=PC;
+		// FETCH
+		// T0
+		AR = PC;
 
 		word testInstruction = MEMORY[AR];
 		PC++;
@@ -148,7 +151,7 @@ int main()
 
 		string symbol = decodeInstruction(testInstruction);
 
-		//EXECUTION
+		// EXECUTION
 		executeInstruction(symbol);
 	}
 	return 0;
