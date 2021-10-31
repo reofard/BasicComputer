d1d43c4fe952855f6ce0a4d7e2701e69d061f6a1
PAUNME <93248977+PAUNME@users.noreply.github.com>
2021-10-31 19:26:41 +0900

    CLA BUN

--- hexTranslator.cpp
+++ hexTranslator.cpp
@@ -1,60 +1,61 @@
 #include <string>
+#include <iostream>
 
 typedef unsigned char byte;
 typedef unsigned short word;
 
 using namespace std;
 
-// decodeInstruction ÇÔ¼ö¿¡¼­ ·¹Áö½ºÅÍ ÂüÁ¶, ÀÔÃâ·Â¿¬»êÀÇ °æ¿ì Çí»çÄÚµå º¯È¯À» À§ÇØ È£Ãâ
+// decodeInstruction í•¨ìˆ˜ì—ì„œ ë ˆì§€ìŠ¤í„° ì°¸ì¡°, ì…ì¶œë ¥ì—°ì‚°ì˜ ê²½ìš° í—¥ì‚¬ì½”ë“œ ë³€í™˜ì„ ìœ„í•´ í˜¸ì¶œ
 string HexToString(word hex)
 {
 	switch (hex)
 	{
-		// ·¹Áö½ºÅÍ ÂüÁ¶ ¸í·É¾î
-		case 0x7800:
-			return "CLA";
-		case 0x7400:
-			return "CLE";
-		case 0x7200:
-			return "CMA";
-		case 0x7100:
-			return "CME";
-		case 0x7080:
-			return "CIR";
-		case 0x7040:
-			return "CIL";
-		case 0x7020:
-			return "INC";
-		case 0x7010:
-			return "SPA";
-		case 0x7008:
-			return "SNA";
-		case 0x7004:
-			return "SZA";
-		case 0x7002:
-			return "SZE";
-		case 0x7001:
-			return "HLT";
+		// ë ˆì§€ìŠ¤í„° ì°¸ì¡° ëª…ë ¹ì–´
+	case 0x7800:
+		return "CLA";
+	case 0x7400:
+		return "CLE";
+	case 0x7200:
+		return "CMA";
+	case 0x7100:
+		return "CME";
+	case 0x7080:
+		return "CIR";
+	case 0x7040:
+		return "CIL";
+	case 0x7020:
+		return "INC";
+	case 0x7010:
+		return "SPA";
+	case 0x7008:
+		return "SNA";
+	case 0x7004:
+		return "SZA";
+	case 0x7002:
+		return "SZE";
+	case 0x7001:
+		return "HLT";
 
-		// I/O ¸í·É¾î
-		case 0xf800:
-			return "INP";
-		case 0xf400:
-			return "OUT";
-		case 0xf200:
-			return "SKI";
-		case 0xf100:
-			return "SKO";
-		case 0xf080:
-			return "ION";
-		case 0xf040:
-			return "IOF";
+		// I/O ëª…ë ¹ì–´
+	case 0xf800:
+		return "INP";
+	case 0xf400:
+		return "OUT";
+	case 0xf200:
+		return "SKI";
+	case 0xf100:
+		return "SKO";
+	case 0xf080:
+		return "ION";
+	case 0xf040:
+		return "IOF";
 
 	}
 	return "Unknown";
 }
 
-// ¸Ş¸ğ¸® ÂüÁ¶¸í·É
+// ë©”ëª¨ë¦¬ ì°¸ì¡°ëª…ë ¹
 string mHexToString(byte hex)
 {
 	string str = 8 & hex ? "[ Indirect ]" : "[ Direct ]";
@@ -62,19 +63,19 @@ string mHexToString(byte hex)
 
 	switch (opcode)
 	{
-		case 0x0:
-			return str + "AND";
-		case 0x1:
-			return str + "ADD";
-		case 0x2:
-			return str + "LDA";
-		case 0x3:
-			return str + "STA";
-		case 0x4:
-			return str + "BUN";
-		case 0x5:
-			return str + "BSA";
-		case 0x6:
+	case 0x0:
+		return str + "AND";
+	case 0x1:
+		return str + "ADD";
+	case 0x2:
+		return str + "LDA";
+	case 0x3:
+		return str + "STA";
+	case 0x4:
+		return str + "BUN";
+	case 0x5:
+		return str + "BSA";
+	case 0x6:
 		return str + "ISZ";
 	}
 	return "Unknown";
